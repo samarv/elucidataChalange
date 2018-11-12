8e2bca6f335b94ca2070af4caf1a72e74c9fe715
samarv <samarvir1996@gmail.com>
2018-11-12 21:40:18 +0530

    Add file upload button

--- elucidataweb/src/App.js
+++ elucidataweb/src/App.js
@@ -1,16 +1,80 @@
 import React, { Component } from 'react';
+import FileInput from 'react-simple-file-input'
+
 
 import './App.css';
 
+var allowedFileTypes = ["application/json"];
+
+function fileIsIncorrectFiletype(file){
+  if (allowedFileTypes.indexOf(file.type) === -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cancelButtonClicked: false
+    };
+  }
+
+  cancelButtonClicked = () => {
+    return this.state.cancelButtonClicked;
+  }
+
+  resetCancelButtonClicked = () => {
+    this.setState({ cancelButtonClicked: false });
+  }
+
+  showInvalidFileTypeMessage = (file) => {
+    window.alert("Tried to upload invalid filetype " + file.type);
+  }
+
+  showProgressBar = () => {
+    this.setState({ progressBarVisible: true});
+  }
+
+  updateProgressBar = (event) => {
+    this.setState({
+      progressPercent: (event.loaded / event.total) * 100
+    });
+  }
+
+  handleFileSelected = (event, file) => {
+    this.setState({file: file, fileContents: event.target.result});
+    console.log(file)
+    console.log(event.target.result)
+  }
   render() {
     return (
       <div className="App">
         <div className="Container">
-          Elucidata challange
-          <button>
-            Upload JSON File
-          </button>
+
+          <label >
+            <FileInput
+              readAs='binary'
+              style={ { display: 'none' } }
+
+              onLoadStart={this.showProgressBar}
+              onLoad={this.handleFileSelected}
+              onProgress={this.updateProgressBar}
+
+              cancelIf={fileIsIncorrectFiletype}
+              abortIf={this.cancelButtonClicked}
+
+              onCancel={this.showInvalidFileTypeMessage}
+              onAbort={this.resetCancelButtonClicked}
+              />
+
+            <span>
+              Click Here
+            </span>
+
+          </label>
         </div>
       </div>
     );
